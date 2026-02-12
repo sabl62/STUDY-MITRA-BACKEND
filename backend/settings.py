54b4a2c742a0b9e28e9ab08a5c680b96bc074c53
sabal62 <sabalplayz.new@gmail.com>
2026-02-12 23:24:10 +0545

    Configurations for second URL

--- backend/settings.py
+++ backend/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'backend.urls'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5174", "http://localhost:5173", "http://localhost:5175", "https://studymitraa.netlify.app"]
+    "http://localhost:5174", "http://localhost:5173", "http://localhost:5175", "https://studymitraa.netlify.app", "https://study-mitra.netlify.app"]
 
 TEMPLATES = [
     {
